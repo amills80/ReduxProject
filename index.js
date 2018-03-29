77b1d29e3222f0163b694bfb593adaf6c2b63838
Alan Mills <aelonrench@yahoo.com>
2018-03-29 19:06:14 -0400

    Add CSS animation to when scoreboard mounts.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import PlayerReducer from './src/reducers/player';
 import Scoreboard from './src/containers/Scoreboard';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const store = createStore(
   PlayerReducer,
@@ -12,7 +13,15 @@ const store = createStore(
 
 render (
   <Provider store={store}>
+  <ReactCSSTransitionGroup
+          component="ul"
+          transitionName="slide"
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={1000}
+          transitionAppear={500}
+          transitionAppearTimeout={500}>
     <Scoreboard />
+  </ReactCSSTransitionGroup> 
   </Provider>,
   document.getElementById('root')
 );
